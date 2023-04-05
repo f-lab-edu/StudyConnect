92447db0acc2f9cabd024db0cff728416e2f8e87
whale0928 <rlagusrl928@gmail.com>
2023-04-05 21:11:16 +0900

    feat : 프로젝트 초기 설정

--- src/main/java/com/studyconnect/StudyConnectApplication.java
+++ src/main/java/com/studyconnect/StudyConnectApplication.java
@@ -2,10 +2,31 @@ package com.studyconnect;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
+
+import static org.springframework.web.servlet.function.RequestPredicates.GET;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 @SpringBootApplication
 public class StudyConnectApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(StudyConnectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StudyConnectApplication.class, args);
+    }
+
+    /**
+     * Swagger UI. 접속 경로 리다이렉트
+     * Router function.
+     *
+     * @return the router function
+     */
+    @Bean
+    RouterFunction<ServerResponse> routerFunction() {
+        return route(GET("/swagger"), req ->
+                ServerResponse.temporaryRedirect(URI.create("swagger-ui.html")).build());
+    }
+
 }
