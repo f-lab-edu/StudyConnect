73799c78fd9e58845e03f1b38fbcffa2683a7bee
KIM HYUN GI <rlagusrl928@gmail.com>
2023-04-16 21:13:54 +0900

    fix : 파일의 끝은 공백으로 끈나야 한다
    - OS의 새줄 문자를 LF으로 수정

--- src/test/java/studyconnect/StudyConnectApplicationTest.java
+++ src/test/java/studyconnect/StudyConnectApplicationTest.java
@@ -1,17 +1,16 @@
 package studyconnect;
 
-import com.studyconnect.StudyConnectApplication;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.studyconnect.StudyConnectApplication;
 
 @SpringBootTest(classes = StudyConnectApplication.class)
 class StudyConnectApplicationTest {
-    @Test
-    @DisplayName("Spring boot Start Test")
-    void contextLoads() {
-        System.out.println("Spring boot Start Test");
-    }
+	@Test
+	@DisplayName("Spring boot Start Test")
+	void contextLoads() {
+		System.out.println("Spring boot Start Test");
+	}
 }
--- src/test/java/studyconnect/member/dto/LoginRequestTest.java
+++ src/test/java/studyconnect/member/dto/LoginRequestTest.java
@@ -1,13 +1,12 @@
 package studyconnect.member.dto;
 
-
-import com.studyconnect.member.dto.LoginRequest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class LoginRequestTest{
+import com.studyconnect.member.dto.LoginRequest;
 
+public class LoginRequestTest {
 	@Test
 	@DisplayName("LoginRequest 생성 테스트")
 	public void test() {
